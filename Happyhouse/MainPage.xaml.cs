ebf139157a991f7444563ad42ca6644add9d8ad1
ide <ide_bradley@hotmail.com>
2015-05-04 18:33:36 +0100

    classes, more purple, asset

--- Happyhouse/MainPage.xaml.cs
+++ Happyhouse/MainPage.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using Happyhouse;
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
 namespace Happyhouse
