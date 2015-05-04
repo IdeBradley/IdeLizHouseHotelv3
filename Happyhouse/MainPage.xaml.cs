d0d2e5846eb536570de0a5907d3dfe9e9c391013
ide <ide_bradley@hotmail.com>
2015-05-04 21:15:03 +0100

    more layout,  build reservation item in xaml

--- Happyhouse/MainPage.xaml.cs
+++ Happyhouse/MainPage.xaml.cs
@@ -46,10 +46,30 @@ namespace Happyhouse
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
         }
+            private void InsertNewRes( Reservation newRes)
+        {
+            // TODO: Delete or comment the following statement; Mobile Services auto-generates the ID.
+            //Reservation.Id = Guid.NewGuid().ToString();
+
+            //// This code inserts a new TodoItem into the database. When the operation completes
+            //// and Mobile Services has assigned an Id, the item is added to the CollectionView
+            //// TODO: Mark this method as "async" and uncomment the following statement.
+            // await reservationTable.InsertAsync(newRes);
+
+            //items.Add(newRes);
+        }
 
         private void RegisterButton_Click(object sender, RoutedEventArgs e)
         {
-            resultTextBox.Text = "Thank you  for your request.   We will confirm your status by Email";
+             // needs to be updated to allow for a " are you sure " ? question
+             // need a condition on the terms and conditions check box ( also a place to display the details)
+            resultTextBox.Text = " Thank you for requesting a reservation ! ";
+             Random rand = new Random();
+             int name = rand.Next(1, 999999);
+             string resrand = name.ToString();  // roundabout way of generating a unique string key for the Primary Key ;-)
+             var newRes = new Reservation { ReservationID = resrand , PropertyID = "POO1",   ClientID= emailTextBox.Text, BookingStatus = " Requested", TimeSegment = "1", Deposit_Received= false };
+             InsertNewRes(newRes);
         }
+  
     }
 }
